e57f28f59d90523251d6f948f76d34b44cc2f418
abrarr21 <abrarrr2110@gmail.com>
2025-03-07 00:05:36 +0530

    wallpaperViaAPI

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,43 @@
 import React from "react";
 import Sidebar from "./partials/Sidebar";
 import TopNav from "./partials/TopNav";
+import { useState } from "react";
+import axios from "../utils/axios";
+import { useEffect } from "react";
+import Header from "./partials/Header";
 
 const Home = () => {
   document.title = "MovieApp | Homepage";
-  return (
+
+  const [wallpaper, setWallpaper] = useState(null);
+
+  const getWallpaper = async () => {
+    try {
+      const { data } = await axios.get(`/trending/all/day`);
+      let randomdata =
+        data.results[(Math.random() * data.results.length).toFixed()];
+      setWallpaper(randomdata);
+    } catch (error) {
+      console.log("in home, wallpaper fetch error", error);
+    }
+  };
+
+  console.log(wallpaper);
+
+  useEffect(() => {
+    !wallpaper && getWallpaper();
+  }, []);
+
+  return wallpaper ? (
     <>
       <Sidebar />
       <div className="w-[80%] h-full">
         <TopNav />
+        <Header data={wallpaper} />
       </div>
     </>
+  ) : (
+    <h1>Loading...</h1>
   );
 };
 
